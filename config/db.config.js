0e5a86fdbf38889f5e2aff7c6ea46495f84d2856
PFDakotaBenger <103076592+PFDakotaBenger@users.noreply.github.com>
2022-04-11 08:45:17 -0700

    Add files via upload

---
+++ config/db.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "host": "localhost",
+    "username": "postgres",
+    "password": "L@wyer12lawyer12",
+    "database": "sprocket",
+    "dialect": "postgres",
+    "pool": {
+      "max": 5,
+      "min": 0,
+      "acquire": 30000,
+      "idle": 10000
+    },
+    "port": 5432
+}
